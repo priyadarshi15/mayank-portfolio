8e1a0ab41b8a257e6360cea37924dbc6f2d955d9
Mayank Priyadarshi <mayankpriyadarshi15@gmail.com>
2026-06-20 11:58:31 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -116,3 +116,20 @@ document
     });
 
 });
+
+
+
+
+
+
+const hamburger =
+document.querySelector(".hamburger");
+
+const navMenu =
+document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
+});
