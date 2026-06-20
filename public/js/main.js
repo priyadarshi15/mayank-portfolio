ca8b6911326360e13f41f5d6da75a8f62419260b
Mayank Priyadarshi <mayankpriyadarshi15@gmail.com>
2026-06-20 12:05:52 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -122,14 +122,3 @@ document
 
 
 
-const hamburger =
-document.querySelector(".hamburger");
-
-const navMenu =
-document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-
-    navMenu.classList.toggle("active");
-
-});
